540d14191ac891b75a5fd412436f6f749e142390
josestrada123 <mariajosefinaestrada1@gmail.com>
2024-04-11 15:01:44 -0400

    Josefina makes decoder (in Sydney's repository this time)

--- main.py
+++ main.py
@@ -15,8 +15,22 @@ def encoder(num):
         encoded_list.append(encrypted_num_string)
     return "".join(encoded_list)
 
+def decoder(num):
+    num = list(num)
+    decoded_list=[]
+    for term in list(num):
+        if int(term) == 2:
+            decoded_list.append("9")
+        elif int(term) == 1:
+            decoded_list.append("8")
+        elif int(term) == 0:
+            decoded_list.append("7")
+        else:
+            decoded_list.append(str(int(term)-3))
+    return "".join(decoded_list)
 
 if __name__ == "__main__":
+    encoded = ""
     while True:
         print("Menu\n"
               "-------------\n"
@@ -30,6 +44,6 @@ if __name__ == "__main__":
             encoder(password)
             print("Your password has been encoded and stored!\n")
         elif option == 2:
-            pass
+            print("Your encoded password is", encoded, "and the original password is", decoder(encoded))
         elif option == 3:
             break
